c339e96cc7ebe1117d0ad0b83b02fa2cf0e6dd58
Grant Crofton <grantac@hotmail.com>
2016-08-02 17:47:54 +0100

    Added slides

---
+++ Language Demo.rkt
@@ -0,0 +1,30 @@
+; Scheme Language Demo
+
+; Atoms & Lists (& S-Expressions?)
+
+; Atoms - data types, #t & #f
+
+; Lists - data vs function applications
+
+; Functions - prefix notation
+
+; Testing for equality (eq?, =, atom?, number?) (predicates)
+
+; Lists - recursion, car & cdr, cons.  (car & cdr don't work on empty lists).
+
+; Parentheses
+
+; Conditionals - Conf & If (demo - fizzbuzz)
+
+; Recursion (demo - fibonnaci)
+
+; Dynamic typing
+
+; Define-ing values
+
+; Lambdas
+
+; First-class functions
+
+; Macros
+
