7525d20fcf05050156f29f69655ed1b08dfc03d2
Grant Crofton <grantac@hotmail.com>
2016-08-04 14:50:24 +0100

    finished symbol grouper

--- solutions/grant/symbol-grouper.rkt
+++ solutions/grant/symbol-grouper.rkt
@@ -1,21 +1,23 @@
+; assume:
+;  the input is well formed
+;  the first item is always a symbol
+;  the non-symbols are always integers
+
 (define (group-symbols a-list)
 
   (define (make-symbol-list list)
     (cond
       ((null? list) '())
-      ((integer? (car list)) (cons (car list) (make-symbol-list (cdr list))))
+      ((integer? (car list)) (cons (car list)
+                                   (make-symbol-list (cdr list))))
       (else '())))
   
   (cond
     ((null? a-list) '())
-    ((symbol? (car a-list)) (list (car a-list) (make-symbol-list (cdr a-list))))
+    ((symbol? (car a-list)) (cons (list (car a-list) (make-symbol-list (cdr a-list)))
+                                  (group-symbols (cdr a-list))))
     (else (group-symbols (cdr a-list)))))
 
-; assume:
-     ;the input is well formed
-     ;the first item is always a symbol
-     ;the non-symbols are always integers?
-
-(if (eqv? (group-symbols '(a 1 2 3 b 4 5 c d 8 9 e)) '((a (1 2 3)) (b (4 5)) (c ()) (d (8 9)) (e ())))
-    "Test Passed"
+(if (equal? (group-symbols '(a 1 2 3 b 4 5 c d 8 9 e)) '((a (1 2 3)) (b (4 5)) (c ()) (d (8 9)) (e ())))
+    "Test Passed!"
     "Test Failed!")
