b3fb7ab2dc3b48e7b1a76d9dfe0cc1f5afc30fef
jamespi <james.pittendreigh@ee.co.uk>
2016-08-04 09:06:00 +0100

    Adding James & Robertas' solutions for the first two exercises

---
+++ solutions/Robertas_and_James/FizzBuzz.scm
@@ -0,0 +1,26 @@
+(define (fizzBuzz number)
+  (cond
+    
+    ((and (=(remainder number 3) 0) (= (remainder number 5) 0)) '"FIZZBUZZ")
+    ((= (remainder number 3) 0) '"FIZZ")
+    ((= (remainder number 5) 0) '"BUZZ")
+    (else number))
+)
+
+(fizzBuzz 2)
+(fizzBuzz 9)
+(fizzBuzz 10)
+(fizzBuzz 15)
+
+(display "positive dividend and divisor\n")
+(modulo 9 2)
+(remainder 9 2)
+(display "positive dividend and negative divisor\n")
+(modulo 9 -2)
+(remainder 9 -2)
+(display "negative dividend and positive divisor\n")
+(modulo -9 2)
+(remainder -9 2)
+(display "negative dividend and divisor\n")
+(modulo -9 -2)
+(remainder -9 -2)
